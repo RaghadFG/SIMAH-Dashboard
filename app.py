71e456ea91da9cb1b620cf39f143921dce2bddbb
Raghad F. Alghulayqah <raghad.alghulayqah@alraedah.finance>
2022-05-09 09:32:11 +0300

    simah add

--- app.py
+++ app.py
@@ -1,41 +1,51 @@
 import dash
 import dash_design_kit as ddk
-import dash_core_components as dcc
+from dash import dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
 import base64
-
-import datetime
+from datetime import datetime
 import io
-
+import theme
 import dash
 from dash.dependencies import Input, Output, State
-from dash import dcc, html, dash_table
+
+
 
 import pandas as pd
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
 
 
 
 app = dash.Dash(__name__)
-server = app.server  # expose server variable for Procfile
+server = app.server 
 
-df = px.data.stocks()
 
-app.layout = html.Div([
-     ddk.Header([
+app.layout = ddk.App([
+    ddk.Header([
         ddk.Logo(src=app.get_asset_url('logo.png')),
         ddk.Title('SIMAH Dashbord'),
+    ]),
+    html.Div([
+    ddk.Card([
+    ddk.Row(children=
+    [html.Div([
+        html.P('How to use:'),
+        html.P('-'),
+        html.P("Policies & Legal requirements:"),
+        html.P('-'),
+
+    ])]
+    )
     ]),
     dcc.Upload(
         id='upload-data',
         children=html.Div([
             'Drag and Drop or ',
-            html.A('Select Files')
-        ]),
-        style={
+            html.A('Select File')
+        ]),style={
             'width': '100%',
             'height': '60px',
             'lineHeight': '60px',
@@ -48,58 +58,26 @@ app.layout = html.Div([
         # Allow multiple files to be uploaded
         multiple=True
     ),
-    html.Div(id='output-data-upload'),
+   
+         html.Button("Download Text", id="btn-download-txt"),
+         dcc.Download(id="download-text")
+    
 ])
+])
+month=datetime.now().date().month  
+Fname=str(month)+"th-month SIMAH Report.txt"
+@app.callback(
+    Output("download-text", "data"),
+    Input("btn-download-txt", "n_clicks"),
+    prevent_initial_call=True,
+)
+def func(n_clicks):
+    return dict(content="not yet", filename=Fname)
 
-def parse_contents(contents, filename, date):
-    content_type, content_string = contents.split(',')
 
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')))
-        elif 'xls' in filename:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
 
-    return html.Div([
-        html.H5(filename),
-        html.H6(datetime.datetime.fromtimestamp(date)),
-
-        dash_table.DataTable(
-            df.to_dict('records'),
-            [{'name': i, 'id': i} for i in df.columns]
-        ),
-
-        html.Hr(),  # horizontal line
-
-        # For debugging, display the raw contents provided by the web browser
-        html.Div('Raw Content'),
-        html.Pre(contents[0:200] + '...', style={
-            'whiteSpace': 'pre-wrap',
-            'wordBreak': 'break-all'
-        })
-    ])
-
-@app.callback(Output('output-data-upload', 'children'),
-              Input('upload-data', 'contents'),
-              State('upload-data', 'filename'),
-              State('upload-data', 'last_modified'))
-def update_output(list_of_contents, list_of_names, list_of_dates):
-    if list_of_contents is not None:
-        children = [
-            parse_contents(c, n, d) for c, n, d in
-            zip(list_of_contents, list_of_names, list_of_dates)]
-        return children
-
-################3
+####
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
