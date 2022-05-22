92babad6bef8c3942facb09f4f4a96ecd2792363
Raghad Alghulayqah <56214908+RaghadFG@users.noreply.github.com>
2022-05-22 12:41:46 +0300

    add date_format method

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -8,26 +8,29 @@ legal_type_mapping = {'Chamber of ': "CCI", 'Female': "CCI",'General ':'GPA','In
  'Establishment': "LBS",
 'General Partnership':'GPA','Insurance ':'ISO',
 'Joint Stock ':'JSC','Licensed ':'UNK','Limited ':'LPA','Literary, ':'UNK',"Male":'CCI','Unknown':'UNK',' ':'UNK'}
-#
+
+def date_format(df,column):
+    df[column]= pd.to_datetime(df[column])
+    df[column] = df[column].dt.strftime("%Y%m%d")
+    df[column] = df[column].fillna(0).astype(int)
+    return df[column]
+
+
 def preprocessing_df(df):
     
     df = df.replace({"Legal Type": legal_type_mapping})
-    df['CloseDate']= pd.to_datetime(df['CloseDate'])
-    df['CloseDate'] = df['CloseDate'].dt.strftime("%Y%m%d")
-    df['CloseDate'] = df['CloseDate'].fillna(0).astype(int)
-    df['SettleDate']= pd.to_datetime(df['SettleDate'])
-    df['SettleDate'] = df['SettleDate'].dt.strftime("%Y%m%d")
-    df['SettleDate'] = df['SettleDate'].fillna(0).astype(int)
-    df['StartDate']= pd.to_datetime(df['StartDate'])
-    df['StartDate'] = df['StartDate'].dt.strftime("%Y%m%d")
-    df['StartDate'] = df['StartDate'].fillna(0).astype(int)
-    df['LastPaymentDate']= pd.to_datetime(df['LastPaymentDate'])
-    df['LastPaymentDate'] = df['LastPaymentDate'].dt.strftime("%Y%m%d")
-    df['LastPaymentDate'] = df['LastPaymentDate'].fillna(0).astype(int)
-    df['SatisfactionDate']= pd.to_datetime(df['SatisfactionDate'])
-    df['SatisfactionDate'] = df['SatisfactionDate'].dt.strftime("%Y%m%d")
-    df['SatisfactionDate'] = df['SatisfactionDate'].fillna(0).astype(int)
+    df['CloseDate']=date_format(df,'CloseDate')
+   
+    df['SettleDate']= date_format(df,'SettleDate')
+    
+    df['StartDate']= date_format(df,'StartDate')
+    
+    df['LastPaymentDate']= date_format(df,'LastPaymentDate')
+    
+    df['SatisfactionDate']= date_format(df,'SatisfactionDate')
+    
     df['installment_amount'] = df['InstallmentAmount'].fillna(0).astype(int)
+    
     df["Legal Type"]=df["Legal Type"].fillna('UNK')
    
     return df
